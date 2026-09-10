f7b92817d7003b68f6f8115de885b7b43c80da8a
Vivek Singh <viveksingh2k04@gmail.com>
2026-09-10 11:53:43 +0530

    Time: 1 ms (56.32%), Space: 45.4 MB (63.38%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,22 @@
+class Solution {
+    int ans=0;
+
+    public int averageOfSubtree(TreeNode root) {
+        solve(root);
+        return ans;
+    }
+    public int[] solve(TreeNode root){
+        if(root==null){
+            return new int[]{0,0};
+        }
+        int[]left=solve(root.left);
+        int[]right=solve(root.right);
+        
+        int sum = left[0] + right[0] + root.val;
+        int count =left[1] + right[1] +1;
+        if(sum/count==root.val){
+            ans++;
+        }
+        return new int[]{sum,count};
+    }
+}
